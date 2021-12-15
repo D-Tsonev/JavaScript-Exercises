a9f5cda99ed1c7b2ccfc1c0d0e17efc0d1882e4b
Dimitar <dconev@gmail.com>
2021-12-15 21:33:17 +0000

    objects

--- objects.js
+++ objects.js
@@ -3,6 +3,11 @@ const user = {
   age: 26,
   isVaccinated: true,
   hobbies: ['gym', 'travelling', 'cooking'],
+  pet: {
+    type: 'Dog',
+    name: 'Monty',
+    age: '3',
+  },
 }
 const age = 'age'
 const randomVariable = 'name'
@@ -13,4 +18,12 @@ console.log(user.name + 'is' + 'user.age' + 'years')
 console.log('User age => ', user[age])
 console.log('User age => ', user['age'])
 console.log('User age => ', user[randomVariable])
-console.log('User favourite hobby->',user.hobbies[1])
+console.log('User favourite hobby->', user.hobbies[1])
+console.log('User pet type->', user.pet.type)
+console.log(user.pet)
+
+if (user.isVaccinated) {
+  console.log('Welcome to Greece')
+} else {
+  console.log('Two meters please')
+}
