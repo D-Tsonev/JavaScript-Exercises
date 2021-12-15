e952ede1bbf3e9c07a92e6d4eacd5e9525c06d55
Dimitar <dconev@gmail.com>
2021-12-15 21:19:26 +0000

    objects

--- objects.js
+++ objects.js
@@ -1,12 +1,16 @@
 const user = {
   name: 'Dimi',
   age: 26,
+  isVaccinated: true,
+  hobbies: ['gym', 'travelling', 'cooking'],
 }
 const age = 'age'
+const randomVariable = 'name'
 
 // const randomVariable='name'
 
 console.log(user.name + 'is' + 'user.age' + 'years')
 console.log('User age => ', user[age])
 console.log('User age => ', user['age'])
-console.log('User age => ', user[name])
+console.log('User age => ', user[randomVariable])
+console.log('User favourite hobby->',user.hobbies[1])
