3080867af3233d955176e34fcc7c12e4c042e934
Dimitar <dconev@gmail.com>
2021-12-15 19:44:48 +0000

    objects

---
+++ objects.js
@@ -0,0 +1,12 @@
+const user = {
+  name: 'Dimi',
+  age: 26,
+}
+const age = 'age'
+
+// const randomVariable='name'
+
+console.log(user.name + 'is' + 'user.age' + 'years')
+console.log('User age => ', user[age])
+console.log('User age => ', user['age'])
+console.log('User age => ', user[name])
