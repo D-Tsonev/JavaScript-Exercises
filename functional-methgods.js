f9928d48b0252cb3b0ccae5a1e4853b4edc4142f
Dimitar <dconev@gmail.com>
2021-12-15 13:50:26 +0000

    functional methods

--- functional-methgods.js
+++ functional-methgods.js
@@ -88,3 +88,49 @@ function isHereBeACapital(array) {
 const answer = isHereBeACapital(["London", "Barcelona", "Valetta"])
 const anotherAnswer = isHereBeACapital(["Paris", "Zagreb", "Helsinki"])
 console.log("some->", answer, anotherAnswer)
+
+function oddOrEven(array) {
+  return array.map((number) => (number % 2 === 0 ? "even" : "odd"))
+}
+
+const checkEvenOrOdd = oddOrEven([1, 2, 3, 4, 5, 6])
+console.log("odd or even in my array", checkEvenOrOdd)
+
+function beginsWithB(array) {
+  return array.filter((word) => word[0] === "B" || word[0] === "b")
+}
+
+const words = beginsWithB(["apple", "banana", "orange", "Boy"])
+console.log("check if the word start with B or b", words)
+
+function multipyBy10(array) {
+  return array.map((number) => Math.abs(number * 10))
+}
+
+const multiplyNumbers=multipyBy10([1,2,-3,4,5,6,7])
+console.log('multiply numbers in array', multiplyNumbers)
+
+function findIndexx(val,array){
+  const indexOfVal = array.indexOf(val)
+  return indexOfVal>=0 ? indexOfVal : 'Not found'
+}
+
+const names=findIndexx('Alex',['Jim','Alex','John'])
+console.log ('Index of given value in a array', names)
+
+function divisibleBy3(array){
+  return array.some(num => num%3===0)
+}
+
+const checkdevidevBy3 = divisibleBy3([1,2,3,4,5,6,7,8,9])
+console.log('Devided by 3', checkdevidevBy3)
+
+function sumOfArray(array){
+  const sum =array.reduce((acc,num)=> {
+    return acc + parseFloat(num) 
+  },0)
+  return sum
+}
+
+const sumOfArrayResult=sumOfArray([1,2.1,3.6])
+console.log('sum of array', sumOfArrayResult)
