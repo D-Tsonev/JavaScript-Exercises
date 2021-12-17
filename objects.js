d3e7025eb0b26e900e9c10bf97b013f8e142d246
Dimitar <dconev@gmail.com>
2021-12-17 23:04:58 +0000

    objects

--- objects.js
+++ objects.js
@@ -8,9 +8,23 @@ const user = {
     name: 'Monty',
     age: '3',
   },
+  greeting: function () {
+    console.log('Hey')
+  },
+  greetingTwo() {
+    console.log('Hey you')
+  },
+  addHobby() {
+    this.hobbies.push('codding')
+  },
 }
 const age = 'age'
 const randomVariable = 'name'
+const someVariable = user.name
+const misteryPropartName = 'age'
+console.log('mistery Propart Name ->', user[misteryPropartName])
+
+console.log('Some variable is ', someVariable)
 
 // const randomVariable='name'
 
@@ -21,9 +35,54 @@ console.log('User age => ', user[randomVariable])
 console.log('User favourite hobby->', user.hobbies[1])
 console.log('User pet type->', user.pet.type)
 console.log(user.pet)
+user.greeting()
+user.greetingTwo()
+console.log('before addHobby function', user.hobbies)
+user.addHobby()
+console.log('after calling the adHobby funtion', user.hobbies)
 
 if (user.isVaccinated) {
   console.log('Welcome to Greece')
 } else {
   console.log('Two meters please')
 }
+
+const seiClass = {}
+
+seiClass.instructur = 'Nick'
+console.log(seiClass)
+
+seiClass['secondInstructor'] = 'Jack'
+console.log(seiClass)
+
+const recipe = {
+  name: 'Pizza',
+  ingredients: ['cheese', 'tomato', 'basil'],
+  rating: 5,
+}
+
+delete recipe.name
+console.log(recipe)
+
+recipe.ingredients.push('garlic')
+console.log(recipe)
+
+recipe.ingredients.forEach((ingrediant) => console.log(ingrediant))
+
+const batman = {
+  realName: 'Bruce Wayne',
+  fakeName: 'Batman',
+  costume: [],
+  sayHello() {
+    console.log(
+      `Hello I am ${this.realName}, but by night I put tights in and fight crime as ${this.fakeName}`
+    )
+  },
+  getDressed(clothes) {
+    this.costume.push(clothes)
+  },
+}
+
+batman.sayHello()
+batman.getDressed('cape')
+console.log(batman.costume)
