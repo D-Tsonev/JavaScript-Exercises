1942651cfcf1569b1eef1bb8ad7202a37916c2ac
Dimitar <dconev@gmail.com>
2022-01-29 16:17:33 +0000

    static page lab

---
+++ React /react-page-lab/react-page-lab-start/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App() {
+  const headerData = {
+    title: 'Sargent Portraits of Artists & Friends',
+    subtitle: 'by John Sergent',
+  }
+
+  const infoData = {
+    hours: [
+      { day: 'Monday', times: '10.00am - 5.50pm' },
+      { day: 'Tuesday', times: '10.00am - 5.30pm' },
+      { day: 'Wednesday', times: '10.00am - 5.30pm' },
+      { day: 'Thursday', times: '10.00am - 5.30pm' },
+      { day: 'Friday', times: '10.00am - 9.00pm' },
+      { day: 'Saturday', times: '10.00am - 9.00pm' },
+      { day: 'Sunday', times: '10.00am - 5.30pm' },
+    ],
+    admissions: [
+      { type: 'Adults', amount: 25 },
+      { type: 'Seniors', amount: 17 },
+      { type: 'Students', amount: 12 },
+    ],
+  }
+
+  const donorData = {
+    contact: {
+      email: 'development@metmuseum.org',
+      phone: '212-650-2425',
+    },
+    amounts: [50, 100, 250, 500, 1000, 5000],
+  }
+
+  return (
+    <main>
+      <h1>The Metropolitan Museum of Art</h1>
+      <hr />
+      <Header {...headerData} />
+      <hr />
+      {/* <GeneralInfo {...infoData} /> */}
+      <hr />
+      {/* <DonorInfo {...donorData} /> */}
+    </main>
+  )
+}
+
+export default App
---
+++ React /react-page-lab/react-page-lab-start/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import image from '../assets/hero.jpg'
+
+function Header (){
+
+  return (
+    <h1>Hello </h1>
+  )
+}
+
+export default Header
