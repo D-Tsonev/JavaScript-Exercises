4c3ba4f5c21f69a48af88a0978e82de50bee0807
Dimitar <dconev@gmail.com>
2022-01-31 14:21:55 +0000

    Header  Hero

---
+++ React /react-static-hw/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className="section" id="hero">
+      <h1 className="title">Coffee Assembly</h1>
+    </section>
+  )
+}
+
+export default Hero
---
+++ React /react-static-hw/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function  NavBar() { 
+  return ( 
+    <header>
+      <nav className="container">
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#menus">Menu</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#hero">Home</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+  
+export default NavBar
+
+
