52ecd5e9daaf60914c9aaf9f9470302dc46b22f9
Dimitar <dconev@gmail.com>
2022-02-03 16:33:34 +0000

    todo list start

--- React /todo-list/src/App.js
+++ React /todo-list/src/App.js
@@ -1,18 +1,43 @@
-import React,{useState} from 'react';
-import './App.css';
-import Header from './components/Header';
+import React, { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
 
 function App() {
-const [toDoList,setToDoList]=useState()
+  const [toDoList, setToDoList] = useState([
+    {
+      task: 'Make an app',
+      completed: false,
+    },
+  ])
 
+  const handleToggle = (itemToUpdate) => {
+    console.log('item to update', itemToUpdate)
 
+    const updatedList = toDoList.map((item) => {
+      console.log('item in map', item)
+      if (item === itemToUpdate) {
+        console.log('match')
+      }
+    })
+  }
 
   return (
-    <div className="App">
-      Hello World! A To Do List Will Go here!
-      <Header/>
-    </div>
-  );
+    <>
+      <div className="container"></div>
+      <h1>To Do List</h1>
+      {toDoList.map((item, index) => {
+        return (
+          <li
+            className={item.completed ? 'completed' : ''}
+            onClick={() => handleToggle(item)}
+            key={index}
+          >
+            {item.task}
+          </li>
+        )
+      })}
+    </>
+  )
 }
-  
-export default App;
+
+export default App
