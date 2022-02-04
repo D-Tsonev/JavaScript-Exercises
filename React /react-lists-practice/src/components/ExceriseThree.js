3a688947a6b18d835a3bef744661312c87bb3d2a
Dimitar <dconev@gmail.com>
2022-02-04 01:12:19 +0000

    function Pokemon Card

--- React /react-lists-practice/src/components/ExceriseThree.js
+++ React /react-lists-practice/src/components/ExceriseThree.js
@@ -4,6 +4,16 @@ import data from '../data/data'
 // * Each card should be made up of a parent "div", containing an "h3" for the pokemons name, and "img" tag for the pokemons sprite, and a "p" tag containing the pokedex entry text
 // * 🤖 Remember to add a "key" prop to the outer most element created from a "map", it can be any type of value, but must be UNIQUE for every item.
 
+function PokemonCard({ pockemon }) {
+  return (
+    <div key={pockemon.number}>
+      <h3>{pockemon.name}</h3>
+      <img src={pockemon.sprite} alt={pockemon.name} />
+      <p>{pockemon.pokedexEntry}</p>
+    </div>
+  )
+}
+
 function ExerciseThree() {
   const pokemonData = data
   console.log(pokemonData)
@@ -11,15 +21,9 @@ function ExerciseThree() {
   return (
     <div className="ex-3">
       <h1>3</h1>
-      {pokemonData.map(pockemon => 
-        (
-          <div key={pockemon.number}>
-            <h3>{pockemon.name}</h3>
-            <img src={pockemon.sprite} alt={pockemon.name}/>
-            <p>{pockemon.pokedexEntry}</p>
-          </div>
-        ))
-      }
+      {pokemonData.map((pockemon) => (
+        <PokemonCard key={pockemon.number} pockemon={pockemon} />
+      ))}
     </div>
   )
 }
